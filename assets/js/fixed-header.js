1ce3a89eea2321172fe9f92a5cbe9876e04f0409
Yaroslav <benito1988@bk.ru>
2018-07-12 00:03:32 +0300

    mark up

---
+++ assets/js/fixed-header.js
@@ -0,0 +1,15 @@
+$(function(){
+    var shrinkHeader = 300;
+     $(window).scroll(function() {
+       var scroll = getCurrentScroll();
+         if ( scroll >= shrinkHeader ) {
+              $('.header').addClass('fixed');
+           }
+           else {
+               $('.header').removeClass('fixed');
+           }
+     });
+   function getCurrentScroll() {
+       return window.pageYOffset || document.documentElement.scrollTop;
+       }
+   });
